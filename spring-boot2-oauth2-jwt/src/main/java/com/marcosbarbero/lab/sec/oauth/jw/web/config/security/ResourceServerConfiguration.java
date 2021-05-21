293a90de7aaf02fba4145e14d6a9fae1452a4e8d
Srij07 <50131768+Srij07@users.noreply.github.com>
2021-05-21 17:55:05 +0530

    oAuth Test

---
+++ spring-boot2-oauth2-jwt/src/main/java/com/marcosbarbero/lab/sec/oauth/jw/web/config/security/ResourceServerConfiguration.java
@@ -0,0 +1,31 @@
+package com.marcosbarbero.lab.sec.oauth.jw.web.config.security;
+
+import java.security.KeyPair;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import com.marcosbarbero.lab.sec.oauth.jw.web.config.props.SecurityProperties;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    private final TokenStore tokenStore;
+
+    public ResourceServerConfiguration(final TokenStore tokenStore) {
+        this.tokenStore = tokenStore;
+    }
+
+    @Override
+    public void configure(final ResourceServerSecurityConfigurer resources) {
+        resources.tokenStore(tokenStore);
+    }
+
+}
